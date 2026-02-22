f6fa6f8faf0dd281e5dc459a14c9d0bc4763f527
rdgonzaga <rainerdgonzaga@gmail.com>
2026-02-23 00:36:55 +0800

    feat: add payload formats; refactor: change err to error

--- src/protocol.py
+++ src/protocol.py
@@ -13,7 +13,7 @@ MSG_ACK         = 0x02
 MSG_DATA        = 0x03
 MSG_FIN         = 0x04
 MSG_FIN_ACK     = 0x05
-MSG_ERR         = 0x06
+MSG_ERROR         = 0x06
 
 # flags
 FLAG_NONE       = 0x00
@@ -23,7 +23,9 @@ FLAG_EOF        = 0x01
 HEADER_FORMAT   = '!B B I I I H'
 HEADER_SIZE     = struct.calcsize(HEADER_FORMAT)  
 
-
+SYN_PAYLOAD_FORMAT = '!B H B'
+ERR_PAYLOAD_FORMAT = '!B B'
+SYN_ACK_PAYLOAD_FORMAT = '!B H I'
 
 # packs a full protocol packet (header and yung payload)
 def build_packet(msg_type: int, session_id: int, seq: int, ack: int,
@@ -59,6 +61,39 @@ def parse_packet(raw: bytes) -> dict:
         'payload':     payload,
     }
 
+# packs the syn payload 
+def build_syn_payload(op: int, chunk_size: int, filename: str) -> bytes:
+    filename_bytes = filename.encode('utf-8')
+    base = struct.pack(SYN_PAYLOAD_FORMAT, op, chunk_size, len(filename_bytes))
+    return base + filename_bytes
+
+# unpacks the syn payload
+def parse_syn_payload(payload: bytes) -> dict:
+    op, chunk_size, name_len = struct.unpack(SYN_PAYLOAD_FORMAT, payload[:4])
+    filename = payload[4:4+name_len].decode('utf-8')
+    return {'op': op, 'chunk_size': chunk_size, 'filename': filename}
+
+# packs the error payload
+def build_err_payload(err_code: int, msg: str) -> bytes:
+    msg_bytes = msg.encode('utf-8')
+    base = struct.pack(ERR_PAYLOAD_FORMAT, err_code, len(msg_bytes))
+    return base + msg_bytes
+
+# unpacks the error payload
+def parse_err_payload(payload: bytes) -> dict:
+    err_code, msg_len = struct.unpack(ERR_PAYLOAD_FORMAT, payload[:2])
+    msg = payload[2:2+msg_len].decode('utf-8')
+    return {'error_code': err_code, 'msg': msg}
+
+# packs the syn ack payload
+def build_syn_ack_payload(status: int, chunk_size: int, isn: int) -> bytes:
+    return struct.pack(SYN_ACK_PAYLOAD_FORMAT, status, chunk_size, isn)
+
+# unpacks the syn ack payload
+def parse_syn_ack_payload(payload: bytes) -> dict:
+    status, chunk_size, isn = struct.unpack(SYN_ACK_PAYLOAD_FORMAT, payload[:7])
+    return {'status': status, 'chunk_size': chunk_size, 'isn': isn}
+
 # just makes the types a string for easier readability sa terminal
 def type_to_str(msg_type: int) -> str:
     names = {
@@ -68,6 +103,6 @@ def type_to_str(msg_type: int) -> str:
         MSG_DATA:    'DATA',
         MSG_FIN:     'FIN',
         MSG_FIN_ACK: 'FIN_ACK',
-        MSG_ERR:     'ERR',
+        MSG_ERROR:   'ERROR',
     }
     return names.get(msg_type, f'UNKNOWN(0x{msg_type:02x})')
